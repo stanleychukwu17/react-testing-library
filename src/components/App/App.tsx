666566bd875022378b9e3f28185d70e44d76c3e6
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-05-25 08:05:24 +0100

    brought in the boilerplate files for the site styling     brought in the utilities scss files that will be used across the site

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import './app.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppMain">
         this is good!
     </div>
   );
